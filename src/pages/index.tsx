e68aa336ea71b22e4f61f0923ca802ddb8a43c99
Ali <muhammadaliqorbanzadeh@gmail.com>
2023-04-09 17:57:48 +0300

    Add new favicon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,7 +12,7 @@ export default function Home() {
         <title>Multi Step Form</title>
         <meta name="description" content="Frontend Mentor Challenge" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon-32x32.png" />
       </Head>
       <Layout>
         <Form />
